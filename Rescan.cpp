efbb3b8c2735ac383907c2b2dde8676b50e89097
jeffreyjean <126814712+jeffreyjean@users.noreply.github.com>
2025-05-02 09:09:12 -0700

    Update new sample code
    basically working

--- Rescan.cpp
+++ Rescan.cpp
@@ -8,10 +8,14 @@
 #include <devguid.h>
 
 #pragma comment(lib, "SetupAPI.lib")
-void EnumerateDevices(DEVINST devInst) {
+DEVINST xHciDevInst = 0;
+WCHAR * xHCI_Host_Controller = (WCHAR *) L"ACPI\\QCOM0D08\\3";
+// Test
+//WCHAR* xHCI_Host_Controller = (WCHAR*)L"URS\\VEN_QCOM&DEV_0C8B&SUBSYS_CRD08380";
+
+void EnumerateDevices(DEVINST devInst, BOOLEAN rescan) {
     CONFIGRET cr;
     DEVINST childDevInst = 0;
-    DEVINST xHciDevInst = 0;
     DEVINST siblingDevInst = 0;
     WCHAR deviceID[MAX_DEVICE_ID_LEN];
 
@@ -22,31 +26,37 @@ void EnumerateDevices(DEVINST devInst) {
     // Traverse all children
     do {
         // Get device ID string
+        ZeroMemory(&deviceID[0], sizeof(deviceID));
+        //Get Device Instance Path: "ACPI\QCOM0D08\3"
         cr = CM_Get_Device_ID(childDevInst, deviceID, MAX_DEVICE_ID_LEN, 0);
-        //printf("CM_Get_Device_ID : %ls\n",deviceID);
+        
+        //if (rescan) 
+        //    printf("CM_Get_Device_ID : %ls\n", deviceID);
         if (cr == CR_SUCCESS) {
             // Filter for USB devices
-            //if (wcsncmp(deviceID, L"ACPI\\VEN_QCOM&DEV_0D08", 22) == 0) {
-            if (wcsncmp(deviceID, L"ACPI\\VEN_QCOM&DEV_06A1", 8) == 0) {
-                printf("Found ACPI USB xHCI Controller : %ls\n",deviceID);
+            // ACPI\\QCOM0D08\\3: KB/TP
+            // ACPI\\QCOM0D09\\4: Fingerprint reader
+
+            if ((!rescan)&&(wcsncmp(deviceID, xHCI_Host_Controller, wcslen(xHCI_Host_Controller)) == 0)) {
+                //printf("Found USB xHCI Controller!\n");
                 xHciDevInst = childDevInst;
-                //break;
+                break;
             }
         }
-    
+
         // Recurse into this child node to find deeper USB devices
-        EnumerateDevices(childDevInst);
+        EnumerateDevices(childDevInst, rescan);
 
         // Get the next sibling
         cr = CM_Get_Sibling(&childDevInst, childDevInst, 0);
-        
+    
     } while (cr == CR_SUCCESS);
 }
 
 int main() {
     DEVINST rootDevInst;
     CONFIGRET cr;
-
+    /*
     // Get device info set for all present USB devices
     HDEVINFO hDevInfo = SetupDiGetClassDevs(
         &GUID_DEVCLASS_USB,
@@ -58,7 +68,7 @@ int main() {
         return 1;
     }
     unsigned int counter = 0;
-    while (true)
+    while (false)
     {
         // Trigger a device tree rescan
         CM_Reenumerate_DevNode(NULL, 0);
@@ -66,14 +76,14 @@ int main() {
         Sleep(1000);
         counter++;
         printf("[%3d] Rescan device tree and sleep for 1 second\n\n", counter);
-        if (counter == 10)
+        if (counter == 5)
         {
             printf("Reach counter limit %d, stop the recan daemon\n", counter);
             break;
         }
     }
     SetupDiDestroyDeviceInfoList(hDevInfo);
-
+    */
     // Locate root devnode
     cr = CM_Locate_DevNode(&rootDevInst, NULL, CM_LOCATE_DEVNODE_NORMAL);
     if (cr != CR_SUCCESS) {
@@ -81,8 +91,18 @@ int main() {
         return 1;
     }
 
-    printf("Enumerating USB devices...\n");
-    EnumerateDevices(rootDevInst);
+    EnumerateDevices(rootDevInst, FALSE);
+
+    if (xHciDevInst)
+    {
+        unsigned int counter = 0;
+        do {
+            //printf("\n\Scan child devices (ELAN KB/TP controller) behind USB xHCI Host Controller...");
+            EnumerateDevices(xHciDevInst, TRUE);
+            counter++;
+            Sleep(50);
+        } while (TRUE);
+    }
     return 0;
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
